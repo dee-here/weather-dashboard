3b286d6ed249dba03facca68e4e271e738a138e2
Dee <deepakdilse@gmail.com>
2023-10-10 01:22:05 -0700

    Add functionality to search and load cities from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 //handle to DOM elements
 //var searchBtnEl = document.querySelector("#search-btn");
 var searchTextEl = document.querySelector("#search-text");
+var forecastWeatherContainerEl = document.querySelector('#forecast-container');
+var todaysWeatherContainerEl = document.querySelector('#todays-weather');
+
+var citiesContainerEl = document.getElementById("saved-cities-container");
+var fiveDayForecastEL = document.getElementById("five-day-forecast-text");
 
 //global variables
 var apiKey = '84f1f04fe0b05f9704f36a0db8c8b0cf';
-var city = 'San Francisco'
+var city = 'San Francisco';
+
+//curent date
+var today = dayjs();
 
 var latitude;
 var longitude;
@@ -16,26 +24,53 @@ var savedCities;
 //update this array and save to local storage everytime the user clicks on searc and we have a valid city !!
 
 class DayForecast {
-    constructor(day, icon, temp, wind) {
+    constructor(day, icon, temp, wind, humidity) {
         this.day = day;
         this.icon = icon; //use as a location for img tag
         this.temp = temp;
         this.wind = wind;
+        this.humidity = humidity;
     }
 }
 
-var forCastArray;
+var foreCastArray;
+var todayWeather;
 
-function getCordinates(data) {
-    console.log("get Cordinates: ", data);
-}
+// function getCordinates(data) {
+//     console.log("get Cordinates: ", data);
+// }
 
-function updateSavedCities() {
+function updateSavedCities(searchedCity) {
     //when user click on search, save the city to local storage. if length of saved string is more than 8; remove the first itme and push ina the new city and save to localstorage
+    savedCities = JSON.parse(localStorage.getItem('citiesArray')) || [];
+    console.log("savedCities is: ", savedCities);
+
+    if(searchedCity && !savedCities.includes(searchedCity.trim())) {
+        if(savedCities?.length > 7) {
+            savedCities.shift();
+        }
+        savedCities.push(searchedCity);
+        localStorage.setItem('citiesArray', JSON.stringify(savedCities));
+    }
+    displaySavedCities();
+
 }
 
 function displaySavedCities() {
-    
+    savedCities = JSON.parse(localStorage.getItem('citiesArray')) || [];
+    console.log("savedCities TO DISPLAY ::: ", savedCities);
+    citiesContainerEl.innerHTML = '';
+    //create button for each saved city
+    savedCities.forEach((city) => {
+        console.log('city is: ', city);
+        let button = document.createElement('button');
+        button.textContent = city;
+        button.classList.add('btn', 'btn-primary', 'btn-block');
+        button.addEventListener('click', savedCityClicked);
+        citiesContainerEl.appendChild(button);
+        //create a button event listener for button clicks?
+    });
+
 }
 
 
@@ -44,82 +79,195 @@ function displaySavedCities() {
 function getGeolocationApi() {
     //geo location
     // var city = "San francisco"
-    var geoCodingApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    var geoCodingApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
     fetch(geoCodingApiUrl)
     .then(function (response) {
-        console.log("Geo response is : ", response );
-        return response.json();
+        console.log("Geo response is : ", response, response.ok );
+        if(response.ok) {
+            return response.json();
+        } else {
+            // alert("No data to display !!" + response.statusText);
+            ///clear contents of forecast and todays weather container !!
+            forecastWeatherContainerEl.innerHTML = '';
+            todaysWeatherContainerEl.innerHTML = '';
+        }
     })
     .then(function (data) {
-        console.log("Geo data : ", data);
-        //thing = data;
-        // get cordinates !
-        // latitude = Math.abs((data?.coord?.lat)?.toFixed(2));
-        // longitude = Math.abs((data?.coord?.lon)?.toFixed(2));
-        latitude = data?.coord?.lat;
-        longitude = data?.coord?.lon;
-        console.log("found coords for : ", data?.name, " lat: ", latitude, " longitude: ", longitude );
-        getWeatherApi();
+        if(data?.name) {
+            console.log("Geo data : for ", data.name, "  ", data);
+            //TODO
+            //save this city to local storagesss
+            updateSavedCities(data.name);
+
+            getTodaysWeatherDetails(data);
+            displayTodaysWeather();
+            latitude = data?.coord?.lat;
+            longitude = data?.coord?.lon;
+            console.log("found coords for : ", data?.name, " lat: ", latitude, " longitude: ", longitude );
+
+            //get its weather forecast.
+            getWeatherApi();
+        }
+
     })
 
 }
 
 function getWeatherApi() {
-    //var requestUrl = 'https://api.github.com/orgs/nodejs/repos'; api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-    //weather !!
-
-    //Id is determining the city.. change to latitude and longitude once geolocation funx ready.
-    //var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`;
     var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-    var thing;
-
-    //get the geo cordinates for the city before getting weather!
 
     fetch(requestUrl)
         .then(function (response) {
             console.log("response is : ", response );
-            return response.json();
+            if(response.ok) {
+                return response.json();
+            } else {
+                // alert("No data to display !!" + response.statusText);
+                forecastWeatherContainerEl.innerHTML = '';
+                todaysWeatherContainerEl.innerHTML = '';
+            }
         })
         .then(function (data) {
-            console.log("weather data : ", data);
-            //maybe not modify datsa!
-            data = data?.list;
-            foreCastArray = [];
-            for (let i=3; i< 39; i=i+8) {
-                var day = data[i]?.dt_txt;
-                var icon = data[i].weather[0]?.icon;
-                var temp = data[i].main?.temp;
-                var wind = data[i].wind?.speed + " MPH";
-
-                foreCastArray.push( new DayForecast(day, icon, temp,wind));
-            }
-            console.log("forecast is: ", foreCastArray);
+            console.log("Forecast weather data : ", data);
+            get5daysForecast(data?.list);
+            // Display 5 days forecast !
+            displayForecast();
         })
 
 }
 
-function get5daysForecast() {
-
+function get5daysForecast(weatherDataList) {
         //iterate over weather data and populate each day with index starting at 3 and + 8 for next days noon !
+        foreCastArray = [];
+            for (let i=0; i< weatherDataList?.length; i=i+8) {
+                let day = weatherDataList[i]?.dt_txt;
+                //format date to be consistent!
+                day = dayjs(day.split(' ')[0]).format("MMMM D, YYYY");
+                let icon = weatherDataList[i].weather[0]?.icon;
+                let temp = weatherDataList[i].main?.temp;
+                let wind = weatherDataList[i].wind?.speed + " MPH";
+                let humidity = weatherDataList[i].main?.humidity;
+                foreCastArray.push( new DayForecast(day, icon, temp, wind, humidity));
+            }
+}
+
+function displayForecast() {
+    // var forecastWeatherContainerEl = document.querySelector('#forecast-container');
+    forecastWeatherContainerEl.innerHTML = '';
+    if(foreCastArray?.length > 0) {
+        fiveDayForecastEL.classList.remove('hidden');
+        foreCastArray.forEach(forecast => {
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('forecast');
+        
+            let newPara = document.createElement('p');
+            let newParaText = document.createTextNode(forecast.day);
+            newPara.appendChild(newParaText);
+        
+            let newImage = document.createElement('img');
+            newImage.src = `http://openweathermap.org/img/w/${forecast.icon}.png`;
+        
+            let newParaTemp = document.createElement('p');
+            let newParaTempText = document.createTextNode(`Temp: ${forecast.temp} \u00B0F`);
+            newParaTemp.appendChild(newParaTempText);
+        
+            let newParaWind = document.createElement('p');
+            let newParaWindText = document.createTextNode(`Wind: ${forecast.wind}`);
+            newParaWind.appendChild(newParaWindText);
+        
+            let newParaHumidity = document.createElement('p');
+            let newParaHumidityText = document.createTextNode(`Wind: ${forecast.humidity}`);
+            newParaHumidity.appendChild(newParaHumidityText);
+        
+            newDiv.appendChild(newPara);
+            newDiv.appendChild(newImage);
+            newDiv.appendChild(newParaTemp);
+            newDiv.appendChild(newParaWind);
+            newDiv.appendChild(newParaHumidity);
+            forecastWeatherContainerEl.appendChild(newDiv);
+        });
+       }
+}
+
+function getTodaysWeatherDetails(weatherData) {
+    console.log("getTodaysWeatherDetails with : ", weatherData);
+    if(weatherData) {
+    let day = city + " " + today.format("MMMM D, YYYY");
+    let icon = weatherData.weather[0]?.icon;
+    let temp = weatherData.main?.temp;
+    let wind = weatherData.wind?.speed + " MPH";
+    let humidity = weatherData.main?.humidity;
+    todayWeather = new DayForecast(day, icon, temp, wind, humidity);
+    }
+
+}
+
+function displayTodaysWeather() {
+//    var todaysWeatherContainerEl = document.querySelector('#todays-weather');
+   console.log("todaysWeatherContainerEl : ", todaysWeatherContainerEl);
+   //clear the contents of the div.
+   todaysWeatherContainerEl.innerHTML = '';
+   if(todayWeather) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('forecast');
+
+    let newPara = document.createElement('p');
+    let newParaText = document.createTextNode(todayWeather.day);
+    newPara.appendChild(newParaText);
+
+    let newImage = document.createElement('img');
+    newImage.src = `http://openweathermap.org/img/w/${todayWeather.icon}.png`;
+
+    let newParaTemp = document.createElement('p');
+    let newParaTempText = document.createTextNode(`Temp: ${todayWeather.temp} \u00B0F`);
+    newParaTemp.appendChild(newParaTempText);
+
+    let newParaWind = document.createElement('p');
+    let newParaWindText = document.createTextNode(`Wind: ${todayWeather.wind}`);
+    newParaWind.appendChild(newParaWindText);
+
+    let newParaHumidity = document.createElement('p');
+    let newParaHumidityText = document.createTextNode(`Wind: ${todayWeather.humidity}`);
+    newParaHumidity.appendChild(newParaHumidityText);
+
+    newDiv.appendChild(newPara);
+    newDiv.appendChild(newImage);
+    newDiv.appendChild(newParaTemp);
+    newDiv.appendChild(newParaWind);
+    newDiv.appendChild(newParaHumidity);
+    todaysWeatherContainerEl.appendChild(newDiv);
+   }
 
 }
 
 function getSearchResults(event) {
-    console.log("event is: ", event);
-    console.log("event is: ", searchTextEl);
+    // console.log("event is: ", event);
+    // console.log("event is: ", searchTextEl);
     city = (searchTextEl.value).trim();
-    console.log("Current city is: ", city);
+    // console.log("Current city is: ", city);
     if(city) {
         getGeolocationApi();
     } else {
-        alert("Please enter a valid city name!");
+        //instead of alert .. displaya  new div with "API error".
+        //alert("Please enter a valid city name!");
+        forecastWeatherContainerEl.innerHTML = '';
+        todaysWeatherContainerEl.innerHTML = '';
     }
+}
 
-    // getApi();
+function savedCityClicked() {
+    // city = this.textContent;
+    console.log('button event: ', event);
+    console.log('this : ', this);
+    city = this.textContent;
+    console.log('City is: ', city);
+    getGeolocationApi();
 }
 
 //main funtion
 function init() {
-
+    displaySavedCities();
 }
+
+init();
